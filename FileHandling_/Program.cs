6bc8671078da01c0551e2e51d3feb74c4580a04a
m.yovchevski <m.yovchevski@LAB108PC23>
2023-09-26 14:58:05 +0300

    changes26/sept

--- FileHandling_/Program.cs
+++ FileHandling_/Program.cs
@@ -5,8 +5,6 @@ using System.Text;
 using System.Collections.Generic;
 using System.Collections;
 
-
-
 class Test
 {
     public static void Main()
@@ -36,7 +34,7 @@ class Test
         var file = $"{path}/file.txt"; //interpolation 
         //File.Create(file) ;
 
-
+      
 
         //another way of doing the same 
         var docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
@@ -46,27 +44,51 @@ class Test
         // GET INFO ABOUT DIRECTORIES
 
         DirectoryInfo di = Directory.CreateDirectory(dirName) ;
-
+  
         Console.ForegroundColor = ConsoleColor.Blue;  
-        Console.WriteLine($"Full name:{di.Name}, the directory has been created at {di.CreationTime}");
+        Console.WriteLine($"Full name:{di.Name}, the directory has been created at {di.CreationTime}, {di.Parent}");
         Console.ResetColor();
 
 
-        //simple reading and writing on/from files 
+        //simple reading and writing ON/FROM Files 
 
 
-        string[] names = { "Monica", "Chandler", "Rachel" };
+        string[] names = { "Monica", "Chandler", "Rachel","Peter" };
+
 
         File.WriteAllLines(file, names) ;
+        File.AppendAllLines(file, names) ; //add info to the file 
 
-
+        //reading from the file in realtime 
         foreach ( string item in File.ReadLines(file) ) 
         
         {
             Console.WriteLine($"Data is:{item}");
         }
 
+        //Search for certain files on PC
+        string[] fileDir = Directory.GetDirectories(docPath);
 
+        foreach( var item in fileDir )
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(item);
+            Console.ResetColor ();
+        }
+
+        //Specific option to search
+
+        DirectoryInfo myDataDir = new DirectoryInfo(docPath);
+
+        //initialize an array of type File Info so we can store the information about the files and then go through them 
+        FileInfo[] txtFiles = myDataDir.GetFiles("*.jpg", SearchOption.AllDirectories);
+
+        //Print the actual files from the PC
+        foreach (var j in txtFiles)
+        {
+            Console.WriteLine(j.Name); //gives the name of the file 
+            Console.WriteLine(j.Length); // gives he length of the file in bytes 
+        }
     }
 
 }
